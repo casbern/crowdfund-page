1aa6faf5c920cd6cd29aadbd43101948f3177218
Cássia <cascrisbern@gmail.com>
2025-08-05 16:26:41 +0300

    added responsive cards component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Button  from "./components/Button"
 
 import icon from "./assets/icon.svg"
 import bookmarkIcon from "./assets/bookmark-icon.svg"
+import Card from "./components/Card"
 
 function App() {
 
@@ -92,10 +93,36 @@ function App() {
             Placing your monitor at eye level has the potential to improve your posture 
             and make you more comfortable while at work, helping you stay focused on the task at hand.
           </p>
-          <p className="text-txt mt-7">
+          <p className="text-txt mt-7 mb-10">
             Featuring artisan craftsmanship, the simplicity of design creates extra desk space 
             below your computer to allow notepads, pens, and USB drives to be stored under the stand.
           </p>
+
+          <Card 
+            title="Bamboo Stand"
+            pledge="25"
+            description="You get an ergonomic stand made of natural bamboo. 
+            You've helped us launch our promotional campaign, and you’ll 
+            be added to a special Backer member list."
+            left="101"
+          />
+
+          <Card 
+            title="Black Edition Stand"
+            pledge="75"
+            description="You get a Black Special Edition computer stand and a personal thank you. 
+            You’ll be added to our Backer member list. Shipping is included."
+            left="64"
+          />
+
+          <Card 
+            title="Mahogany Special Edition"
+            pledge="200"
+            description="You get two Special Edition Mahogany stands, a Backer T-Shirt, 
+            and a personal thank you. You’ll be added to our Backer member list. Shipping is included. "
+            left="0"
+          />
+          
         </div>
 
       </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,29 @@
 import Button from "./Button"
 
-const Card = () => {
+interface CardProps {
+  title: string;
+  pledge: string;
+  description: string;
+  left: string;
+}
+
+const Card = ({title, pledge, description, left}: CardProps) => {
   return (
-    <div>
-      <div>
-        <h3>Bamboo Stand</h3>
-        <span>Pledge $25 or more</span>
+    <div className="border border-border rounded-lg p-9 mb-6">
+      <div className="flex flex-col md:flex-row items-start md:items-center md:justify-between mb-6">
+        <h3 className="text-lg font-bold">{title}</h3>
+        <span className="text-sm font-medium text-call-action">Pledge $${pledge} or more</span>
       </div>
 
-      <p>
-        You get an ergonomic stand made of natural bamboo. 
-        You've helped us launch our promotional campaign, and you’ll 
-        be added to a special Backer member list.
+      <p className="text-txt mb-6 text-base font-normal">
+        {description}
       </p>
 
-      <div>
-        <span> <strong>101</strong> left</span>
+      <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
+        <div className="flex items-center gap-2  mb-6 md:mb-0">
+          <strong className="text-3xl font-bold">{left}</strong> 
+          <span className="text-txt text-sm font-normal">left</span>
+        </div>
         <Button>Select Reward</Button>
       </div>
     </div>
